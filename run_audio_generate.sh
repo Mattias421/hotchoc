b139743364bf6be7c999a6e4ecda993775b867a4
Mattias Cross <mcross2@sheffield.ac.uk>
2026-03-05 13:23:03 +0000

    Change user dir

--- run_audio_generate.sh
+++ run_audio_generate.sh
@@ -4,10 +4,9 @@ RESULTS_PATH=$3
 
 cd $FAIRSEQ_ROOT/examples/wav2vec/unsupervised
 
-PYTHONPATH=$FAIRSEQ_ROOT python ~/w2vu_tools/w2vu_generate.py --config-dir config/generate --config-name viterbi \
-fairseq.common.user_dir=${FAIRSEQ_ROOT}/examples/wav2vec/unsupervised \
+PYTHONPATH=$FAIRSEQ_ROOT python ~/w2vu_tools/w2vu_generate.py --config-dir config/generate --config-name audio \
+fairseq.common.user_dir=${HOME}/hotchoc \
 fairseq.task.data=$DATA \
-fairseq.task._name=train_audio \
 fairseq.common_eval.path=$CKPT/checkpoint_best.pt \
 fairseq.dataset.gen_subset=valid results_path=$RESULTS_PATH \
 hydra.run.dir=$EXP/outputs/hydra
